c3374ebfa9dc545427235904cdb67df83bd593c4
Brad Root <bradroot@me.com>
2021-12-06 18:25:55 -0800

    Initial Commit

---
+++ Internet Time/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Internet Time
+//
+//  Created by Brad Root on 12/6/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Internet Time/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Internet Time
+//
+//  Created by Brad Root on 12/6/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
